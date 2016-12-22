fac1bb5410ac79d29256bd49d43ecfe042c2c8b5
mastersthesisobinna <masterthesis.obinnamogbogu@gmail.com>
2016-12-21 21:46:37 -0600

    21days

--- LearnJava21Days/src/Chapter1/Chess.java
+++ LearnJava21Days/src/Chapter1/Chess.java
@@ -13,11 +13,8 @@ public class Chess {
     int startY;//start position on a X,Y axis
     int x1, x2, x3, x4, x5, x6, x7, x8;
     int y;
-    String objectColor;//Object Color: 0 is black, 1 is white
-    String squareColor1;
-    String squareColor2;
-    String objectName;
-    
+    String objectColor;//Object Color: 0 is black, 1 is white    
+    String objectName;    
     
     public Chess(String name, String color){
         objectName = name;
@@ -33,89 +30,101 @@ public class Chess {
     }
     
     public void squareColor(){
+
+	String squareColor1;
+    	String squareColor2;
+
         if(objectColor.equalsIgnoreCase("black")){
             if(objectName.equalsIgnoreCase("king")){
                 squareColor1 = "white";
-                squareColor2 = "NA";
+                squareColor2 = "NA";//only one black king exist on the chess
                 System.out.println("Square color 1: "+squareColor1);
                 System.out.println("Square color 2: "+squareColor2);
             }
             if(objectName.equalsIgnoreCase("queen")){
                 squareColor1 = "black";
-                squareColor2 = "NA";
+                squareColor2 = "NA";//only one black king exist on the chess
                 System.out.println("Square color 1: "+squareColor1);
                 System.out.println("Square color 2: "+squareColor2);
             }
             if(objectName.equalsIgnoreCase("rook")){
                 //from left to right of board
-                squareColor1 = "white";
-                squareColor2 = "black";
+                squareColor1 = "white";//first piece on the chess LHS
+                squareColor2 = "black";//second piece on the chess RHS
                 System.out.println("Square color 1: "+squareColor1);
                 System.out.println("Square color 2: "+squareColor2);
             }
             if(objectName.equalsIgnoreCase("bishop")){
                 //from left to right of board
-                squareColor1 = "white";
-                squareColor2 = "black";
+                squareColor1 = "white";//first piece on the chess LHS
+                squareColor2 = "black";//second piece on the chess RHS
                 System.out.println("Square color 1: "+squareColor1);
                 System.out.println("Square color 2: "+squareColor2);
             }
             if(objectName.equalsIgnoreCase("knight")){
                 //from left to right of board
-                squareColor1 = "black";
-                squareColor2 = "white";
+                squareColor1 = "black";//first piece on the chess LHS
+                squareColor2 = "white";//second piece on the chess RHS
                 System.out.println("Square color 1: "+squareColor1);
                 System.out.println("Square color 2: "+squareColor2);
             }
             if(objectName.equalsIgnoreCase("pawn")){
                 //from left to right of board
-                squareColor1 = "black";
-                squareColor2 = "white";
+                squareColor1 = "black";//first piece on the chess LHS
+                squareColor2 = "white";//second piece on the chess RHS
                 System.out.println("Square color 1: "+squareColor1);
                 System.out.println("Square color 2: "+squareColor2);
             }
+	    else{
+		//chess object does not exist
+		System.out.println("Invalid bject: Object does not exist for a chess game");
+	    }
         }
         else if(objectColor.equalsIgnoreCase("white")){
             if(objectName.equalsIgnoreCase("king")){
                 squareColor1 = "black";
-                squareColor2 = "NA";
+                squareColor2 = "NA";//only one White king exist on the chess
                 System.out.println("Square color 1: "+squareColor1);
                 System.out.println("Square color 2: "+squareColor2);
             }
             if(objectName.equalsIgnoreCase("queen")){
                 squareColor1 = "white";
-                squareColor2 = "NA";
+                squareColor2 = "NA";//only one White queen exist on the chess
                 System.out.println("Square color 1: "+squareColor1);
                 System.out.println("Square color 2: "+squareColor2);
             }
             if(objectName.equalsIgnoreCase("rook")){
                 //from left to right of board
-                squareColor1 = "black";
-                squareColor2 = "white";
+                squareColor1 = "black";//first piece on the chess LHS
+                squareColor2 = "white";//second piece on the chess RHS
                 System.out.println("Square color 1: "+squareColor1);
                 System.out.println("Square color 2: "+squareColor2);
             }
             if(objectName.equalsIgnoreCase("bishop")){
                 //from left to right of board
-                squareColor1 = "black";
-                squareColor2 = "white";
+                squareColor1 = "black";//first piece on the chess LHS
+                squareColor2 = "white";//second piece on the chess RHS
                 System.out.println("Square color 1: "+squareColor1);
                 System.out.println("Square color 2: "+squareColor2);
             }
             if(objectName.equalsIgnoreCase("knight")){
                 //from left to right of board
-                squareColor1 = "white";
-                squareColor2 = "black";
+                squareColor1 = "white";//first piece on the chess LHS
+                squareColor2 = "black";//second piece on the chess RHS
                 System.out.println("Square color 1: "+squareColor1);
                 System.out.println("Square color 2: "+squareColor2);
             }
             if(objectName.equalsIgnoreCase("pawn")){
                 //from left to right of board
-                squareColor1 = "white";
-                squareColor2 = "black";
+                squareColor1 = "white";//first piece on the chess LHS
+                squareColor2 = "black";//second piece on the chess RHS
                 System.out.println("Square color 1: "+squareColor1);
                 System.out.println("Square color 2: "+squareColor2);
             }
+	    else{
+		//chess object does not exist
+		System.out.println("Invalid bject: Object does not exist for a chess game");
+	    }
         }
         else{
             System.out.println("Error!");
